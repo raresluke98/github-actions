412d44c4943017095c07bdf36c92a69bb906be91
Damian-Luca Rares <rares.luke98@gmail.com>
2021-08-12 11:07:39 +0300

    added bugged test

--- tests/calculations/test_type_checks.py
+++ tests/calculations/test_type_checks.py
@@ -10,3 +10,8 @@ def test_is_float():
     assert is_float(1.0)
     assert not is_float(1)
     assert not is_float('a')
+
+def test_is_float_bugged():
+    assert not is_float(1.0)
+    assert not is_float(1)
+    assert not is_float('a')
